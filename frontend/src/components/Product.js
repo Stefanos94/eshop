6def1dbf83ccd5fea13f954e78d178d6204cdd5d
Stefanos Avramis <stefanosavramis94@gmail.com>
2022-01-12 18:24:02 +0200

    third commit

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,19 @@
+export default function Product(props){
+    const {product} = props;
+    return (
+        <div key={product._id} className="card">
+                <a href={`/product/${product.id}`}>
+                    <img className="medium" src={product.image} alt={product.name}/>
+                </a>
+                <div className="card-body">
+                <a href={`/product/${product.id}`}>
+                        <h2>{product.name}</h2>
+                    </a>
+                    
+                    <div className="price">
+                        {product.price}€
+                    </div>
+                </div>
+            </div>
+    )
+}
